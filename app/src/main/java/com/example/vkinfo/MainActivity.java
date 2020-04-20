27348f9f13341d5f98cb6e9fc295de78f45502bf
Vadim-BG <infotourbg.vadim@gmail.com>
2020-04-20 15:06:57 +0300

    + VK Api + JSON

--- app/src/main/java/com/example/vkinfo/MainActivity.java
+++ app/src/main/java/com/example/vkinfo/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.vkinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,6 +23,43 @@ public class MainActivity extends AppCompatActivity {
     private EditText searchField;
     private Button searchButton;
     private TextView result;
+
+    class VkQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+                response = getResponseFromURL(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            String fistName = null;
+            String lastName = null;
+
+
+            try {
+                JSONObject jsonResponse = new JSONObject(response);
+                JSONArray jsonArray = jsonResponse.getJSONArray("response");
+                JSONObject userInfo = jsonArray.getJSONObject(0);
+
+                fistName = userInfo.getString("first_name");
+                lastName = userInfo.getString("last_name");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            String resultString = "Имя: " + fistName + "\n" + "Фамилия: " + lastName;
+
+            result.setText(resultString);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +74,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 URL generatedURL = generateURL(searchField.getText().toString());
-                String response = null;
-                try {
-                     response = getResponseFromURL(generatedURL);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                result.setText(response);
+                new VkQueryTask().execute(generatedURL);
             }
         };
         searchButton.setOnClickListener(onClickListener);
--- app/src/main/java/com/example/vkinfo/utils/NetworkUtils.java
+++ app/src/main/java/com/example/vkinfo/utils/NetworkUtils.java
@@ -10,16 +10,19 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class NetworkUtils {
-    private static final String VK_API_BASE_URL = "http://api.vk.com";
-    private static final String VK_USERS_GET = "/method/user.get";
+    private static final String VK_API_BASE_URL = "https://api.vk.com";
+    private static final String VK_USERS_GET = "/method/users.get";
     private static final String PARAM_USER_ID = "user_ids";
+    private static final String ACCESS_TOKEN = "access_token";
     private static final String PARAM_VERSION = "v";
 
     public static URL generateURL(String userId) {
         Uri buildUri = Uri.parse(VK_API_BASE_URL + VK_USERS_GET)
                 .buildUpon()
                 .appendQueryParameter(PARAM_USER_ID, userId)
-                .appendQueryParameter(PARAM_VERSION, "5.89")
+                .appendQueryParameter(ACCESS_TOKEN,
+                        "c75718bec75718bec75718bef0c7263a82cc757c75718be99cf680b568a2c36439fb5ce")
+                .appendQueryParameter(PARAM_VERSION, "5.8")
                 .build();
 
         URL url = null;
